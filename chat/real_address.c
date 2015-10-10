c6310a4ad393801ffdf43f931f0d1c014de4e5d3
anpar <paris.antoine.paris@gmail.com>
2015-10-10 22:45:05 +0200

    Try to test real_address

--- chat/real_address.c
+++ chat/real_address.c
@@ -1,41 +1,51 @@
 #include "real_address.h"
 
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 const char * real_address(const char * address, struct sockaddr_in6 * rval) {
     
-      fprintf(stderr,"FONCTION real_address \n");
+        fprintf(stderr,"FONCTION real_address \n");
 
-    struct addrinfo hints;
-    struct addrinfo * result;
-    int s;
+        struct addrinfo hints;
+        struct addrinfo * result;
+        int s;
     
-    /* Initialize hints */
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET6;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = 17;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
+         /* Initialize hints */
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = AF_INET6;
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = 17;
+        hints.ai_flags = AI_PASSIVE;
+        hints.ai_canonname = NULL;
+        hints.ai_addr = NULL;
+        hints.ai_next = NULL;
 	
-    s = getaddrinfo(address, NULL, &hints, &result);
-    if(s != 0)
-        return(gai_strerror(s));
-   
-    rval = (struct sockaddr_in6 *) result->ai_addr;
-    rval = rval;
-    
-   	/* Free result addrinfo */
-    freeaddrinfo(result);
+        s = getaddrinfo(address, NULL, NULL, &result);
+        if(s != 0)
+                return(gai_strerror(s));
+  
+        printf("result->ai_family : %d.\n", result->ai_family);
+        printf("result->ai_socktype : %d.\n", result->ai_socktype);
+        printf("result->ai_protocol : %d.\n", result->ai_protocol);
+        printf("result->ai_flags : %d.\n", result->ai_flags);
+        printf("result->ai_addr->sa_family : %d.\n", result->ai_addr->sa_family);
+        char * dst = (char *) malloc(40*sizeof(char));
+        struct sockaddr_in * saddr = (struct sockaddr_in *) result->ai_addr;
+        const char * t  = inet_ntop(AF_INET6, saddr, dst, 40);
+        if(t == NULL) {printf("error");}
+        printf("result->ai_addr->sa_data : %s.\n", dst);
+
+        rval = (struct sockaddr_in6 *) result->ai_addr;
+        rval = rval;
+        /* Free result addrinfo */
+        freeaddrinfo(result);
 
 	return(NULL);
 }
--- chat/test.c
+++ chat/test.c
@@ -9,17 +9,25 @@
 
 /* Test Suite setup and cleanup functions: */
 
-int init_suite(void) {
-        return 0;
-}
+int init_suite(void) {return 0;}
 
-int clean_suite(void) {     
-        return 0;
-}
+int clean_suite(void) {return 0;}
 
 /* Test case functions */
-void test_real_address(void) {
 
+void test_real_address(void) {
+        struct sockaddr_in6 * rval = (struct sockaddr_in6 *) malloc(sizeof(struct sockaddr_in6));
+        const char * address_1 = "google.com";
+        const char * s = real_address(address_1, rval);
+        
+        CU_ASSERT(s == NULL);
+        printf("rval->sin6_family : %d.\n", rval->sin6_family);
+        printf("rval->sin6_port : %d.\n", rval->sin6_port);
+        printf("rval->sin6_flowinfo : %d.\n", rval->sin6_flowinfo);
+        printf("rval->sin6_addr : %s.\n", rval->sin6_addr.s6_addr);
+        printf("rval->sin6_scope_id : %d.\n", rval->sin6_scope_id);
+        
+        free(rval);
 }
 
 /* Test Runner Code goes here */
