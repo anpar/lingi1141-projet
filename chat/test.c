d4dc62621a887ef9a57c4ba2b50e1cc2071ea91c
anpar <paris.antoine.paris@gmail.com>
2015-10-10 18:57:21 +0200

    Add unit test template

---
+++ chat/test.c
@@ -0,0 +1,52 @@
+#include "real_address.h"
+#include "create_socket.h"
+#include "read_write_loop.h"
+#include "wait_for_client.h"
+
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include <stdlib.h>
+
+/* Test Suite setup and cleanup functions: */
+
+int init_suite(void) {
+        return 0;
+}
+
+int clean_suite(void) {     
+        return 0;
+}
+
+/* Test case functions */
+void test_real_address(void) {
+
+}
+
+/* Test Runner Code goes here */
+int main(void) {
+        CU_pSuite basic = NULL;
+        
+        /* initialize the CUnit test registry */
+        if(CUE_SUCCESS != CU_initialize_registry()) {
+                return(CU_get_error());
+        }
+
+        /* add a suite to the registry */
+        basic = CU_add_suite("Complete test suite", init_suite, clean_suite);
+        if (basic == NULL) {
+                CU_cleanup_registry();
+                return(CU_get_error());
+        }
+
+        /* add the tests to the suite */
+        if      (
+                        (NULL == CU_add_test(basic, "test_real_address", test_real_address))
+                )
+        {
+                CU_cleanup_registry();
+                return CU_get_error();
+        }
+
+        CU_basic_run_tests();
+        return CU_get_error();
+}
