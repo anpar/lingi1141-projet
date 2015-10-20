c7e97dc733c3fbf40bd6581ff6d8d22979351ffb
anpar <paris.antoine.paris@gmail.com>
2015-10-20 23:51:39 +0200

    Fix indent, first version of receiver

--- protocol/src/read_loop.c
+++ protocol/src/read_loop.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>      /* perror, STDOUT_FILENO, fprintf */
 #include <stdbool.h>    /* true, false */
-#include <unistd.h>		/* close */
+#include <unistd.h>		/* close, read, write */
 #include <fcntl.h>		/* open */
 #include <stdlib.h>		/* malloc, free */
 #include <sys/select.h> /* select */
@@ -14,8 +14,8 @@
 
 struct window {
 	pkt_t * buffer[31];
-	int last_in_seq;
-	int free_space;
+	uint8_t last_in_seq;
+	uint8_t free_space;
 };
 
 typedef struct window win;
@@ -28,10 +28,10 @@ win * init_window()
 	win *w = (win *) malloc(sizeof(win));
 	if(w == NULL)
 		return NULL;
-		
+
 	w->last_in_seq = 0;
 	w->free_space = WIN_SIZE;
-	
+
 	return w;
 }
 
@@ -49,20 +49,45 @@ void free_window(win * rwin)
  * Décale la fenêtre et affiche sur out_fd les élements
  * en séquences dans la fenêtre (s'il y en a).
  */
-void shift_window(win * rwin, int out_fd) 
+void shift_window(win * rwin, int out_fd)
 {
-	
+    bool hole_in_window = false;
+
+    int i;
+    for(i = 0; i < WIN_SIZE && !hole_in_window; i++) {
+        if(rwin->buffer[i] != NULL) {
+            // En principe toujours vrai
+            if(pkt_get_seqnum(rwin->buffer[i]) == ((rwin->last_in_seq + 1) % 256)) {
+                rwin->last_in_seq = (rwin->last_in_seq + 1) % 256;
+
+                ssize_t written_on_out = 0;
+                while (written_on_out != pkt_get_length(rwin->buffer[i])) {
+                    ssize_t ret = write (out_fd, (void *) pkt_get_payload(rwin->buffer[i]), pkt_get_length(rwin->buffer[i])-written_on_out);
+                    if (ret == -1) {
+                        perror("write()");
+                    }
+
+                    written_on_out += ret;
+                }
+
+                pkt_del(rwin->buffer[i]);
+                rwin->free_space++;
+            }
+        } else {
+            hole_in_window = true;
+        }
+    }
 }
 
 /*
  * Retourne true si le numéro de séquence
  * rentre dans la fenre, false sinon.
  */
-bool in_window(win * rwin, uint8_t seqnum) 
+bool in_window(win * rwin, uint8_t seqnum)
 {
 	int max = (rwin->last_in_seq + WIN_SIZE) % 256;
 	int min = rwin->last_in_seq;
-	
+
 	/*
 	 * La fenêtre ressemble à ça (valeurs
 	 * possibles = *) :
@@ -71,18 +96,60 @@ bool in_window(win * rwin, uint8_t seqnum)
 	 */
 	if((rwin->last_in_seq + WIN_SIZE) > 256) {
 		return (seqnum > min) || (seqnum <= max);
-	} 
+	}
 	/* La fenêtre ressemble à ça (valeurs
 	 * possibles = *) :
 	 * 		 min      max
 	 * -------*********--------
 	 */
 	else {
-		return (seqnum > min) && (seqnum <= max); 
+		return (seqnum > min) && (seqnum <= max);
 	}
 }
 
-void read_loop(int sfd, char * filename) 
+/*
+ * Construit un ack pour un paquet reçu
+ * FIX: le CRC doit-il se trouver dans un paquet
+ * de type PTYPE_ACK? Pour un PTYPE_NACK c'est explicitement
+ * précisé mais quid dans ce cas-ci?
+ */
+void build_ack(char ack[8], win * rwin) {
+    ack[0] = (PTYPE_ACK << 5) | rwin->free_space;
+	ack[1] = (rwin->last_in_seq + 1) % 256;
+	ack[2] = 0;
+	ack[3] = 0;
+}
+
+/*
+ * Construit un nack pour un paquet reçu
+ */
+void build_nack(pkt_t * pkt, char nack[4], win * rwin) {
+    nack[0] = (PTYPE_NACK << 5) | rwin->free_space;
+    nack[1] = pkt_get_seqnum(pkt);
+    nack[2] = (pkt_get_length(pkt) >> 8) & 0xFF;
+	nack[3] = pkt_get_length(pkt) & 0xFF;
+}
+
+/*
+ * Permet d'écrire les ack et nack sur le socket.
+ * Renvoie true si tout s'est bien passé, false sinon.
+ */
+bool write_on_socket(int sfd, char * buf, int size) {
+    ssize_t written_on_socket = 0;
+    while (written_on_socket != size) {
+        ssize_t ret = write (sfd, (void *) buf, size-written_on_socket);
+        if (ret == -1) {
+            perror("write()");
+            return false;
+        }
+
+        written_on_socket += ret;
+    }
+
+    return true;
+}
+
+void read_loop(int sfd, char * filename)
 {
 	int err;
 	int fd = -1;
@@ -90,7 +157,7 @@ void read_loop(int sfd, char * filename)
 	int retval;
     fd_set readfds, writefds;
     char buf[MAX_PKT_SIZE];
-	
+
 	/* Ouverture de filename s'il est spécifié */
 	if((ff = (filename != NULL))) {
 		fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC);
@@ -99,23 +166,23 @@ void read_loop(int sfd, char * filename)
 			return;
 		}
 	}
-	
+
 	/* Initialisation de la fenêtre de réception */
 	win * rwin = init_window();
-	
+
 	int out_fd;
 	if(ff)
 		out_fd = fd;				// out_fd = fd si ff == true
 	else
 		out_fd = STDOUT_FILENO;		// out_fd = stdout sinon
-	
+
 	while(true)
 	{
 		FD_SET(sfd, &readfds);     	// On peut lire sur le socket
         FD_SET(sfd, &writefds);     // On peut écrire sur le socket
 		FD_SET(out_fd, &writefds);	// On peut écrire sur out_fd
-			
-		retval = select(sfd+1, &readfds, &writefds, NULL, NULL);																							
+
+		retval = select(sfd+1, &readfds, &writefds, NULL, NULL);
         if (retval == -1) {
             perror("select()");
             return;
@@ -126,7 +193,7 @@ void read_loop(int sfd, char * filename)
         else {
 			/* On peut lire (paquet de données) et écrire (ack
 			ou nack correspondant) sur le socket */
-			if(FD_ISSET(sfd, &readfds) && FD_ISSET(sfd, &writefds)) 
+			if(FD_ISSET(sfd, &readfds) && FD_ISSET(sfd, &writefds))
 			{
 				/* On lit les données sur le socket */
 				ssize_t read_on_socket = read(sfd, (void *) buf, MAX_PKT_SIZE);
@@ -134,20 +201,45 @@ void read_loop(int sfd, char * filename)
                     perror("read()");
                     return;
                 }
-                
-				/* On décode les données reçues */
+
+				/* On décode les données reçues dans d_pkt */
                 pkt_t *d_pkt = pkt_new();
                 pkt_status_code c = pkt_decode((const char *) buf, (const size_t) read_on_socket, d_pkt);
                 /* Si le paquet reçu est valide et que le numéro de séquence
                    rentre dans le fenêtre de réception, on le traite */
                 if(c == PKT_OK && in_window(rwin, pkt_get_seqnum(d_pkt)))
                 {
-					
+                    /* Si le packet lu est de type PTYPE_DATA... */
+                    if(pkt_get_type(d_pkt) == PTYPE_DATA)
+                    {
+                        /* ... et qu'il contient plus de 4 bytes. Alors, on
+                        le place dans la fenêtre de réception et on envoie
+                        un ACK */
+                        if(read_on_socket != 4) {
+                            char ack[4];
+                            rwin->free_space--;     // Une place de moins dans rwin
+                            build_ack(ack, rwin);
+                            if(!write_on_socket(sfd, ack, 4))
+                                return;
+
+                            // On place d_pkt dans la fenêtre de réception
+                            rwin->buffer[pkt_get_seqnum(d_pkt) - ((rwin->last_in_seq + 1) % 256)] = d_pkt;
+                        }
+                        /* Sinon, le paquet a été coupé à cause de congestion
+                        dans le réseau, il faut avertir le sender en envoyant
+                        un NACK */
+                        else {
+                            char nack[4];
+                            build_nack(d_pkt, nack, rwin);
+                            if(write_on_socket(sfd, nack, 4))
+                                return;
+                        }
+                    }
 				}
-				
+
 				/* Sinon on ne fait rien, on l'ignore */
 			}
-			
+
 			/* On peut écrire sur out_fd */
 			if(FD_ISSET(out_fd, &writefds))
 			{
@@ -155,10 +247,10 @@ void read_loop(int sfd, char * filename)
 			}
 		}
 	}
-	
+
 	/* Libération de l'espace alloué à la fenêtre de réception */
 	free(rwin);
-	
+
 	/* Femeture du fd de filename s'il a est spécifié */
 	if(ff) {
 		err = close(fd);
--- segments/src/packet_implem.c
+++ segments/src/packet_implem.c
@@ -4,165 +4,165 @@
 #include <zlib.h>
 
 /*
- * Structure reprensenting a packet.
- * Types of the different fields are
- * deduced from the getters/setters
- * defined in the interface.
- */
+* Structure reprensenting a packet.
+* Types of the different fields are
+* deduced from the getters/setters
+* defined in the interface.
+*/
 struct __attribute__((__packed__)) pkt {
 	ptypes_t type : 3;
-        uint8_t window : 5;
-        uint8_t seqnum;
-        uint16_t length;
-        char * payload;
-        uint32_t crc;
+	uint8_t window : 5;
+	uint8_t seqnum;
+	uint16_t length;
+	char * payload;
+	uint32_t crc;
 };
 
 
 pkt_t* pkt_new()
 {
-        pkt_t * pkt = (pkt_t *) (malloc(sizeof(pkt_t)));
-        if(pkt == NULL)
-                return(NULL);
+	pkt_t * pkt = (pkt_t *) (malloc(sizeof(pkt_t)));
+	if(pkt == NULL)
+	return(NULL);
 
-        pkt->payload = NULL;
+	pkt->payload = NULL;
 
-        return(pkt);
+	return(pkt);
 }
 
 void pkt_del(pkt_t *pkt)
 {
-        if(pkt->payload != NULL)
-                free(pkt->payload);
+	if(pkt->payload != NULL)
+	free(pkt->payload);
 
-        if(pkt != NULL)
-                free(pkt);
+	if(pkt != NULL)
+	free(pkt);
 }
 
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
-        if(len < 4)
-	  return(E_NOHEADER);
-	
-        /*
-         * @return says that unless the error is E_NOHEADER,
-         * the packet has at least the values of the header
-         * found in the data stream. So, we first add the
-         * values of the header and then only check the
-         * return values of the setters used.
-         */
-        pkt_status_code c1 = pkt_set_type(pkt, data[0] >> 5);
-        pkt_status_code c2 = pkt_set_window(pkt, data[0] & 0b00011111);
-        pkt_status_code c3 = pkt_set_seqnum(pkt, data[1]);
-        pkt_status_code c4 = pkt_set_length(pkt, (data[2] << 8) | data[3]);
-        
-        if(c1 != PKT_OK) {return(c1);}
-        if(c2 != PKT_OK) {return(c2);}
-        if(c3 != PKT_OK) {return(c3);}
-        if(c4 != PKT_OK) {return(c4);}
-        
-        /*
-         * Le paquet a été coupé par un routeur à cause de la
-         * congestion (mais il est quand même valide).
-         */
-        if(len == 4)
-	  return(PKT_OK);
-			
+	if(len < 4)
+		return(E_NOHEADER);
+
+	/*
+	* @return says that unless the error is E_NOHEADER,
+	* the packet has at least the values of the header
+	* found in the data stream. So, we first add the
+	* values of the header and then only check the
+	* return values of the setters used.
+	*/
+	pkt_status_code c1 = pkt_set_type(pkt, data[0] >> 5);
+	pkt_status_code c2 = pkt_set_window(pkt, data[0] & 0b00011111);
+	pkt_status_code c3 = pkt_set_seqnum(pkt, data[1]);
+	pkt_status_code c4 = pkt_set_length(pkt, (data[2] << 8) | data[3]);
+
+	if(c1 != PKT_OK) {return(c1);}
+	if(c2 != PKT_OK) {return(c2);}
+	if(c3 != PKT_OK) {return(c3);}
+	if(c4 != PKT_OK) {return(c4);}
+
+	/*
+	* Le paquet a été coupé par un routeur à cause de la
+	* congestion (mais il est quand même valide).
+	*/
+	if(len == 4)
+		return(PKT_OK);
+
 	if(len != 4 && pkt_get_type(pkt) != PTYPE_DATA)
-	  return(E_UNCONSISTENT);
-			
-        if(len < 8)
-	  return(E_UNCONSISTENT);
-        
-        uint32_t received_crc = (uint8_t) data[len-4];
-        received_crc = (received_crc << 8) + (uint8_t) data[len-3];
-        received_crc = (received_crc << 8) + (uint8_t) data[len-2];
-        received_crc = (received_crc << 8) + (uint8_t) data[len-1];
-        uint32_t computed_crc = crc32(0L, (Bytef *) data, len-4); 
-        
-        if(received_crc != computed_crc)
-                return(E_CRC);
+		return(E_UNCONSISTENT);
 
-        pkt_status_code c5 = pkt_set_crc(pkt, received_crc);
-        if(c5 != PKT_OK) {return(c5);}
+	if(len < 8)
+		return(E_UNCONSISTENT);
 
-        if(len == 8) {return(E_NOPAYLOAD);}
-        if(len % 4 != 0) {return(E_PADDING);}
+	uint32_t received_crc = (uint8_t) data[len-4];
+	received_crc = (received_crc << 8) + (uint8_t) data[len-3];
+	received_crc = (received_crc << 8) + (uint8_t) data[len-2];
+	received_crc = (received_crc << 8) + (uint8_t) data[len-1];
+	uint32_t computed_crc = crc32(0L, (Bytef *) data, len-4);
 
-        uint16_t padding = (4 - (pkt_get_length(pkt) % 4)) % 4;
-        if((4 + pkt_get_length(pkt) + padding + 4) != (uint16_t) len)
-		 return(E_UNCONSISTENT);
-         
-        if(pkt_get_type(pkt) != PTYPE_DATA && pkt_get_length(pkt) != 0)
-	       	return(E_UNCONSISTENT);
+	if(received_crc != computed_crc)
+		return(E_CRC);
 
-        pkt_status_code c6 = pkt_set_payload(pkt, data+4, pkt_get_length(pkt));
-        if(c6 != PKT_OK)
-                return(c6);
+	pkt_status_code c5 = pkt_set_crc(pkt, received_crc);
+	if(c5 != PKT_OK) {return(c5);}
 
-        return(PKT_OK);
+	if(len == 8) {return(E_NOPAYLOAD);}
+	if(len % 4 != 0) {return(E_PADDING);}
+
+	uint16_t padding = (4 - (pkt_get_length(pkt) % 4)) % 4;
+	if((4 + pkt_get_length(pkt) + padding + 4) != (uint16_t) len)
+		return(E_UNCONSISTENT);
+
+	if(pkt_get_type(pkt) != PTYPE_DATA && pkt_get_length(pkt) != 0)
+		return(E_UNCONSISTENT);
+
+	pkt_status_code c6 = pkt_set_payload(pkt, data+4, pkt_get_length(pkt));
+	if(c6 != PKT_OK)
+		return(c6);
+
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
-        buf[0] = (pkt_get_type(pkt) << 5) | pkt_get_window(pkt);
-        buf[1] = pkt_get_seqnum(pkt);
-        buf[2] = (pkt_get_length(pkt) >> 8) & 0xFF;
-        buf[3] = pkt_get_length(pkt) & 0xFF;
+	buf[0] = (pkt_get_type(pkt) << 5) | pkt_get_window(pkt);
+	buf[1] = pkt_get_seqnum(pkt);
+	buf[2] = (pkt_get_length(pkt) >> 8) & 0xFF;
+	buf[3] = pkt_get_length(pkt) & 0xFF;
 
-        /*
-         * Writing the payload in the buffer. First
-         * condition allows to detect the end of the
-         * payload and second allows to detect the end
-         * of the buffer (taking into account the
-         * fact that we need to keep space for the CRC).
-         */
-        const char * payload = pkt_get_payload(pkt);
-        uint16_t padding = (4 - (pkt_get_length(pkt) % 4)) % 4;
-        uint16_t i; // Represents the number of payload's bytes written in the buffer
-        for(i = 0; i < pkt_get_length(pkt) + padding && i + 4 < ((uint16_t) *len) - 4; i++) {
-                buf[i+4] = payload[i];
-        }
+	/*
+	* Writing the payload in the buffer. First
+	* condition allows to detect the end of the
+	* payload and second allows to detect the end
+	* of the buffer (taking into account the
+	* fact that we need to keep space for the CRC).
+	*/
+	const char * payload = pkt_get_payload(pkt);
+	uint16_t padding = (4 - (pkt_get_length(pkt) % 4)) % 4;
+	uint16_t i; // Represents the number of payload's bytes written in the buffer
+	for(i = 0; i < pkt_get_length(pkt) + padding && i + 4 < ((uint16_t) *len) - 4; i++) {
+		buf[i+4] = payload[i];
+	}
 
-        /*
-         * If the loop stopped due to second
-         * condition, then it means the buffer
-         * is too small and so pkt_encode must
-         * return E_NOMEM.
-         */
-        if(i != pkt_get_length(pkt) + padding)
-                return(E_NOMEM);
+	/*
+	* If the loop stopped due to second
+	* condition, then it means the buffer
+	* is too small and so pkt_encode must
+	* return E_NOMEM.
+	*/
+	if(i != pkt_get_length(pkt) + padding)
+	return(E_NOMEM);
 
-        /*
-         * Compute the CRC and add it at the end
-         * of buf.
-         * TODO: find a way to add this CRC in the
-         * pkt stucture.
-         */
-        uint32_t crc = (uint32_t) crc32(0, (Bytef *) buf, 4 + pkt_get_length(pkt) + padding);
-        buf[i+4] = (crc >> 24) & 0xFF;
-        buf[i+5] = (crc >> 16) & 0xFF;
-        buf[i+6] = (crc >> 8) & 0xFF;
-        buf[i+7] = crc & 0xFF;
+	/*
+	* Compute the CRC and add it at the end
+	* of buf.
+	* TODO: find a way to add this CRC in the
+	* pkt stucture.
+	*/
+	uint32_t crc = (uint32_t) crc32(0, (Bytef *) buf, 4 + pkt_get_length(pkt) + padding);
+	buf[i+4] = (crc >> 24) & 0xFF;
+	buf[i+5] = (crc >> 16) & 0xFF;
+	buf[i+6] = (crc >> 8) & 0xFF;
+	buf[i+7] = crc & 0xFF;
 
-        /*
-         * Total number of bytes written : 4 for the header,
-         * pkt_get_length(pkt) for the useful part of payload,
-         * padding for the padding in the
-         * payload and 4 for the CRC.
-         */
-        *len = 4 + pkt_get_length(pkt) + padding + 4;
-        return(PKT_OK);
+	/*
+	* Total number of bytes written : 4 for the header,
+	* pkt_get_length(pkt) for the useful part of payload,
+	* padding for the padding in the
+	* payload and 4 for the CRC.
+	*/
+	*len = 4 + pkt_get_length(pkt) + padding + 4;
+	return(PKT_OK);
 }
 
 ptypes_t pkt_get_type  (const pkt_t* pkt)
 {
-        return(pkt->type);
+	return(pkt->type);
 }
 
 uint8_t  pkt_get_window(const pkt_t* pkt)
 {
-        return(pkt->window);
+	return(pkt->window);
 }
 
 uint8_t  pkt_get_seqnum(const pkt_t* pkt)
@@ -182,85 +182,85 @@ uint32_t pkt_get_crc   (const pkt_t* pkt)
 
 const char* pkt_get_payload(const pkt_t* pkt)
 {
-       return(pkt->payload);
+	return(pkt->payload);
 }
 
 
 pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
 {
-        if(type != PTYPE_DATA && type != PTYPE_ACK && type != PTYPE_NACK)
-                return(E_TYPE);
+	if(type != PTYPE_DATA && type != PTYPE_ACK && type != PTYPE_NACK)
+	return(E_TYPE);
 
-        pkt->type = type;
-        return(PKT_OK);
+	pkt->type = type;
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_set_window(pkt_t *pkt, const uint8_t window)
 {
-        if(window > MAX_WINDOW_SIZE)
-                return(E_WINDOW);
+	if(window > MAX_WINDOW_SIZE)
+	return(E_WINDOW);
 
-        pkt->window = window;
-        return(PKT_OK);
+	pkt->window = window;
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum)
 {
-        /*
-         * seqnum will never be outside of [0,255] thanks
-         * to its type (uint8_t), so no verification needed.
-         * FIX :
-         */
-        pkt->seqnum = seqnum;
-        return(PKT_OK);
+	/*
+	* seqnum will never be outside of [0,255] thanks
+	* to its type (uint8_t), so no verification needed.
+	* FIX :
+	*/
+	pkt->seqnum = seqnum;
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length)
 {
-        if(length > 512)
-                return(E_LENGTH);
+	if(length > 512)
+	return(E_LENGTH);
 
-        pkt->length = length;
-        return(PKT_OK);
+	pkt->length = length;
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_set_crc(pkt_t *pkt, const uint32_t crc)
 {
-        pkt->crc = crc;
-        return(PKT_OK);
+	pkt->crc = crc;
+	return(PKT_OK);
 }
 
 pkt_status_code pkt_set_payload(pkt_t *pkt, const char *data,
-                                const uint16_t length)
-{
-        pkt_status_code c = pkt_set_length(pkt, length);
-        if(c != PKT_OK)
-                return(c);
+	const uint16_t length)
+	{
+		pkt_status_code c = pkt_set_length(pkt, length);
+		if(c != PKT_OK)
+		return(c);
 
-        uint16_t padding = (4 - (length % 4)) % 4;
+		uint16_t padding = (4 - (length % 4)) % 4;
 
-        /*
-         * If pkt->payload is already allocated
-         * with malloc (!= NULL), then we have
-         * to free'd it before reallocating.
-         */
-        if(pkt->payload != NULL)
-                free(pkt->payload);
+		/*
+		* If pkt->payload is already allocated
+		* with malloc (!= NULL), then we have
+		* to free'd it before reallocating.
+		*/
+		if(pkt->payload != NULL)
+		free(pkt->payload);
 
-        pkt->payload = (char *) malloc((length + padding) * sizeof(char));
-        if(pkt->payload == NULL)
-                return(E_NOMEM);
+		pkt->payload = (char *) malloc((length + padding) * sizeof(char));
+		if(pkt->payload == NULL)
+		return(E_NOMEM);
 
-        int i;
-        for(i = 0; i < length; i++) {
-                pkt->payload[i] = data[i];
-        }
+		int i;
+		for(i = 0; i < length; i++) {
+			pkt->payload[i] = data[i];
+		}
 
-        while(padding > 0) {
-                pkt->payload[i] = '0';
-                i++;
-                padding--;
-        }
+		while(padding > 0) {
+			pkt->payload[i] = '0';
+			i++;
+			padding--;
+		}
 
-        return(PKT_OK);
-}
+		return(PKT_OK);
+	}
